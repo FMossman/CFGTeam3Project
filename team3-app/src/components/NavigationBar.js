d03a8e9dc5de7d3174b87d794c4f3efbfbe8451b
FMossman <fmcmossman@gmail.com>
2023-05-02 19:33:34 +0100

    Started work on a navigation bar

---
+++ team3-app/src/components/NavigationBar.js
@@ -0,0 +1,22 @@
+
+
+
+function NavigationBar() {
+    return (
+        <div className="wholeNavBar">
+            <div className="team3Icon">
+                <img src=".\images\team3logo.png" className="teamLogo" alt="logo of a T with a 3" />
+            </div>
+            <div className="Nav-list">
+                <ul className="navListUl">
+                    <li>Home</li>
+                    <li>Member Page</li>
+                </ul>
+            </div>
+                
+            
+        </div>
+    );
+  }
+  
+  export default NavigationBar;
