5e6ef555883bab974b3895a54d27e80104d55287
FMossman <fmcmossman@gmail.com>
2023-05-11 15:29:16 +0100

    Work on PopUp

--- team3-app/src/components/NavigationBar.js
+++ team3-app/src/components/NavigationBar.js
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 
-
-function NavigationBar() {
+function NavigationBar(props) {
     return (
         <div className="wholeNavBar">
             <div className="team3Icon">
