f256a9e2c30731b4469f718018dd564af5a910dc
Joe McColl <joe4kool@gmail.com>
2023-05-14 14:55:51 +0100

    Adding social icons as dependency, Adding contact detaisl with icons to JodieMember class

--- team3-app/src/pages/MemberJodie.js
+++ team3-app/src/pages/MemberJodie.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import JodieImg from './images/JodieHols.png';
 import './Member.css'
+import { SocialIcon } from 'react-social-icons';
 
-
+const github_url = "https://github.com/Tennjo17"
+const gmail_url = "https://mail.google.com"
+const email_url = "jodietennant17@gmail.com"
+const whatsapp_url = "https://whatsapp.com"
 
 const Member2 = () => {
 return (
@@ -43,18 +47,14 @@ return (
             </div>
             <div className="section-list">
                 <ul>
-                    <li>jodietennant17@gmail.com</li>
-                    <li>07762514233</li>
-                    <li>https://github.com/Tennjo17</li>
+                <li><SocialIcon url={github_url} fgColor="#fff"  />{github_url}</li>
+                <li><SocialIcon url={gmail_url} fgColor="#fff"  />{email_url}</li>
+                <li><SocialIcon url={whatsapp_url} fgColor="#fff"  />07762514233</li>
                 </ul>
             </div>
         </section>
     </div>
 );
 };
-
-
-
-
 
 export default Member2;
