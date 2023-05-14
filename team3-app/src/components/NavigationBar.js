48f1d810488238f0ac63fc470301aa92f38cccca
Joe McColl <joe4kool@gmail.com>
2023-05-14 12:56:32 +0100

    Adding link to homepage within team3Icon, Removing HomePage link from unordered list in Navbar, Adding name text to unordered list in Navbar

--- team3-app/src/components/NavigationBar.js
+++ team3-app/src/components/NavigationBar.js
@@ -4,14 +4,15 @@ function NavigationBar(props) {
     return (
         <div className="wholeNavBar">
             <div className="team3Icon">
+                <Link to="home">
                 <img src=".\images\team3logo.png" className="teamLogo" alt="logo of a T with a 3" />
+                </Link>
             </div>
             <div className="Nav-list">
                 <ul className="navListUl">
-                    <li><Link to="home">Home</Link></li>
-                    <li><Link to="member1">Member 1</Link></li>
-                    <li><Link to="member2">Member 2</Link></li>
-                    <li><Link to="member3">Member 3</Link></li>
+                    <li><Link to="member1">Fiona</Link></li>
+                    <li><Link to="member2">Jodie</Link></li>
+                    <li><Link to="member3">Erin</Link></li>
                 </ul>
             </div>  
         </div>
