2c95f20ea04a832a0fbea91a3439fa5ebd9ff539
FMossman <109220514+FMossman@users.noreply.github.com>
2023-05-18 09:08:26 +0100

    Revert "Linking up the email and editing format "

--- team3-app/src/components/MemberCard.js
+++ team3-app/src/components/MemberCard.js
@@ -1,16 +1,13 @@
 import PropTypes from 'prop-types';
 import './MemberCard.css';
 
+//function MemberCard for holding image/name/age/background
 
-
-function MemberCard({ name, age, picture }) {
+function MemberCard({ name, age }) {
     return(
         <div className="memberCard">
             <h2>{name}</h2>
             <h4>Age: {age}</h4>
-            
-            <img src={picture} className="cardPicture" alt="team member" />
-           
         </div>
     )
 }
